40a05490a3709340b20c04ab7de72b9c7d8c5007
Harry Buck <harry-buck@hotmail.co.uk>
2019-06-07 13:28:42 +0100

    Coded, Tested, Reviewed 'updateClassroom' functionality

--- src/main/java/com/bae/rest/ClassroomController.java
+++ src/main/java/com/bae/rest/ClassroomController.java
@@ -3,6 +3,7 @@ package com.bae.rest;
 import javax.inject.Inject;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -31,8 +32,11 @@ public class ClassroomController {
 
 	}
 
-	public String updateClassroom(int id, String classroom) {
-		return null;
+	@Path("/updateClassroom/{id}")
+	@PUT
+	@Produces({ "application/json" })
+	public String updateClassroom(@PathParam("id") int id, String classroom) {
+		return classroomService.updateClassroom(id, classroom);
 
 	}
 
